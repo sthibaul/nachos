6cd7a23d11f4a00f96f7335da2cd06a0f97f46f8
Samuel Thibault <samuel.thibault@u-bordeaux.fr>
2009-09-24 23:24:50 +0000

    initial version

---
+++ code/threads/bool.h
@@ -0,0 +1,36 @@
+// Defining TRUE and FALSE is usually a Bad Idea,
+// because you will probably be inconsistent with anyone
+// else who had the same clever idea.
+// Therefore:  DON'T USE THIS FILE.
+
+#ifndef _bool_h
+#define _bool_h 1
+
+#ifdef MAC_OS
+
+#include <stdbool.h>
+
+#define TRUE true
+#define FALSE false
+
+#else
+
+#include   <_G_config.h>
+
+#if _G_HAVE_BOOL
+#undef TRUE
+#undef FALSE
+#define TRUE true
+#define FALSE false
+#else
+#undef FALSE
+#undef TRUE
+#undef true
+#undef false
+enum bool
+{ FALSE = 0, false = 0, TRUE = 1, true = 1 };
+#endif
+
+#endif // MAC_OS_SYS
+
+#endif
